5fdc03fddea5f249e0b8de6f203131aa64f6315f
anmol9630 <anmolrai447@gmail.com>
2024-09-21 11:42:19 +0530

    initialcommit

--- CA_Client/src/Components/Auth.jsx
+++ CA_Client/src/Components/Auth.jsx
@@ -147,3 +147,16 @@ const Auth = () => {
 };
 
 export default Auth;
+
+
+
+
+
+
+// echo "# Finance_Manager" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/anmol9630/Finance_Manager.git
+// git push -u origin main
