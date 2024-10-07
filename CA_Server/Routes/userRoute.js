876a9a5fe9b7326c12ab090771cff2197b1b13f8
Shivendra Singh Chandel <chandel486670@gmail.com>
2024-10-07 09:13:30 +0530

    modification in backend

--- CA_Server/Routes/userRoute.js
+++ CA_Server/Routes/userRoute.js
@@ -1,11 +1,8 @@
 const express=require("express");
 const route=express.Router();
-<<<<<<< HEAD
-const {userSignup,userLogin,verifyEmail,Verified,forgotPassword,resetPassword,adminLogin,userInfo}=require("../Controllers/userController");
-=======
-const {userSignup,userLogin,verifyEmail,Verified,forgotPassword,resetPassword,adminLogin}=require("../Controllers/userController");
+
+const {userSignup,userLogin,verifyEmail,forgotPassword,resetPassword,userInfo}=require("../Controllers/userController");
 const { isAuthenticated } = require("../middleware/isAuthenticated");
->>>>>>> 9a700f0c4c0ebf09f6f689c4da591f301bf25490
 
 route.post("/usersignup",userSignup);
 route.get('/me' , userInfo)
--- CA_Server/middleware/isAuthenticated.js
+++ CA_Server/middleware/isAuthenticated.js
@@ -1,11 +1,16 @@
 const userModel=require("../Models/userModel");
+const { ErrorHandler } = require("../utils/ErrorHandlerClass");
 
-export const isAuthenticated=async(req,res,next)=>{
+const isAuthenticated=async(req,res,next)=>{
     const email=req.body.email;
       const user=await userModel.findOne({email:email});
+      if(!user) {return next(new ErrorHandler("User not found"),400)}
       if(!user.verified){
         return next(new ErrorHandler("Please Verify your email first" , 403));
       }
       next();
   }
 
+module.exports={
+  isAuthenticated
+}
